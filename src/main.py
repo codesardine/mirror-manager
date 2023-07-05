c1159f67beb672f1b33d6eb5e9823bff853a2060
Vitor Lopes <vmnlop@gmail.com>
2023-07-05 18:16:28 +0100

    add branch seperation

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from .mirrors.models import Mirror
 
 main = Blueprint("main", __name__)
 
-@main.route("/")
+@main.route("/" , methods=['GET', 'POST'])
 def index():
     mirrors = Mirror().query.filter_by(active=True).order_by(Mirror.country).all()
+    select = request.form.get('arch-select')
+
     return render_template(
         'index.html',
         mirrors=mirrors,
-        total=len(mirrors)
+        total=len(mirrors),
+        select=select
         )
+
